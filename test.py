a6c61b2c65c658e5a2e0427fbefe6959daa83ad7
Flyer King <1257232810.@qq.com>
2020-01-22 14:31:40 +0800

    Jumpy game skeleton

--- test.py
+++ test.py
@@ -1,9 +1,42 @@
-import pygame as py
-print ('hello world') 
+# Pygame template - skeleton fro a new pygame project
+import pygame
+import random
 
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255, 0,0)
+GREEN = (0, 255, 0)
+BLUE = (0,0,255)
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
     
-def funcname(self, parameter_list):
-    print ('xiao laji')
-    print ('tang qi ye yu')
-    print ('laji')
-    pass
+    # Draw / Render
+    screen.fill(BLACK)
+
+    # *after* drawing everything , flip the display
+    pygame.display.flip()
+
+pygame.quit()
